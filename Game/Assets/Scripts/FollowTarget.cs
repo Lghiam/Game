0f2ba1c7aa6def5dda35a56f705e01aa7c082976
Lghiam <liambflynn2@gmail.com>
2018-03-21 23:17:02 +0000

    Project - Game folder

---
+++ Game/Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour {
+
+	//what is being followed
+	public Transform target;
+
+	//zeros out velocity
+	Vector3 velocity = Vector3.zero;
+
+	//time to follow target
+	public float smoothTime = .15f;
+
+	void FixedUpdate(){
+		
+		//target position
+		Vector3 targetPos = target.position;
+
+		//align camera and targets z position
+		targetPos.z = transform.position.z;
+
+		//camera transform position
+		transform.position = Vector3.SmoothDamp (transform.position, targetPos, ref velocity, smoothTime);
+	}
+}
