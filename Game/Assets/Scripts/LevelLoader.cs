36c1527b0ca5c6159c2e5d78c5e82f1c4fcd8b07
Dankennedy97 <danielkennedy496@yahoo.ie>
2018-04-10 16:24:02 +0100

    Able to enter levels from level select, use the w key

---
+++ Game/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour {
+
+	private bool playerInField;
+
+	public string levelLoad;
+
+	// Use this for initialization
+	void Start () {
+		playerInField = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.W) && playerInField) 
+		{
+			Application.LoadLevel (levelLoad);
+		}
+	}
+
+
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.name == "Player") 
+		{
+			playerInField = true;
+		}
+
+	}
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.name == "Player") 
+		{
+			playerInField = false;
+		}
+	}
+}
